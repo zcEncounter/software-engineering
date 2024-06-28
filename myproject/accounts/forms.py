5a0bf71e98f357c8e1bb9d80c35893f42051948d
zcEncounter <2267365907@qq.com>
2024-06-28 23:10:05 +0800

    R4

--- myproject/accounts/forms.py
+++ myproject/accounts/forms.py
@@ -8,3 +8,4 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+#
--- myproject/accounts/models.py
+++ myproject/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+#
 # Create your models here.
 class FishNumber(models.Model):
     total = models.IntegerField()
